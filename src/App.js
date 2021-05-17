e3e61cce93b96dae9027d6995b76b9aeccd7171c
muhammad-junaid1 <mjunaid.swe@gmail.com>
2021-05-18 00:01:09 +0500

    React Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Img from "./picture.png";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React!!!</h1>;
+      <img src={Img} alt="" />
+    </div>
+  );
+};
+
+export default App;
